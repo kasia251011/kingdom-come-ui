f9188241cd4bb492bdab3af5db3e08258897eeb6
Kasia Jankowska <katarzyna.jankowska000728@gmail.com>
2025-01-25 17:07:10 +0100

    Add is on player icon and broken item icon

--- src/components/inventory/GeneralInventory/InventoryListItem/index.tsx
+++ src/components/inventory/GeneralInventory/InventoryListItem/index.tsx
@@ -2,11 +2,11 @@ import { useAppSelector } from "@/redux/hooks";
 import { ItemType } from "@/types/common";
 import { Item } from "@/types/item";
 import { useMemo } from "react";
+import IS_ON_PLAYER from "@/assets/general-inventory/is_on_player.png";
+import BROKEN from "@/assets/general-inventory/broken.png";
 
 const InventoryListItem = ({ item }: { item: Item }) => {
   const { activeInventory, currentItem } = useAppSelector((state) => state.app);
-
-  console.log(currentItem.name);
 
   const isOnPLayer = useMemo(
     () => activeInventory[item.type as ItemType]?.id === item.id,
@@ -25,14 +25,33 @@ const InventoryListItem = ({ item }: { item: Item }) => {
       }`}
     >
       <div className="flex gap-4 items-center">
-        <img src={item.image} className="size-10" />
+        <div className="relative">
+          <img src={item.image} className="size-10" />
+          {isOnPLayer && (
+            <img
+              src={IS_ON_PLAYER}
+              className="size-3.5 absolute bottom-1 -right-2.5"
+            />
+          )}
+          {item.durability < 20 && (
+            <img
+              src={BROKEN}
+              className="size-5 absolute bottom-0.5 -left-2.5"
+            />
+          )}
+        </div>
         <p>{item.name}</p>
       </div>
       <div className="flex items-center pr-4">
-        {isOnPLayer && <div className="size-2 bg-amber-950 rounded" />}
-        <p className="w-16 text-right ">{item.count}</p>
-        <p className="w-16 text-right ">{item.durability}</p>
-        <p className="w-16 text-right "> {item.price}</p>
+        <p className="w-16 text-right font-semibold">{item.count}</p>
+        <p
+          className={`w-16 text-right font-semibold ${
+            item.durability < 20 ? "text-red-500" : ""
+          } `}
+        >
+          {item.durability}
+        </p>
+        <p className="w-16 text-right font-semibold"> {item.price}</p>
       </div>
     </div>
   );
